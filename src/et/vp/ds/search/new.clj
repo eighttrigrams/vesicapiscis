443e6ef5dbcf43f5efe50add7fc8a4cb3d6efaf8
Dan <dan@eighttrigrams.net>
2025-02-20 21:41:40 +0000

    Refactor a bit

--- src/et/vp/ds/search/new.clj
+++ src/et/vp/ds/search/new.clj
@@ -16,21 +16,44 @@
 
 (defn- and-query 
   [{:keys [join-ids]}]
-  (merge
-   {:select :issues.id
-    :from   [:issues]
-    :where  [:and (when
+  [:in :issues.id
+   (merge
+    {:select   :issues.id
+     :from     [:issues]
+     :where    [:and (when
                    ;; TODO review necessity of when condition
-                   join-ids [:in :collections.container_id [:inline join-ids]])]
-    :join     [:collections [:= :issues.id :collections.item_id]]
-    :group-by :issues.id
-    :having   [:raw (str "COUNT(issues.id) = " (count join-ids))]}))
+                      join-ids [:in :collections.container_id [:inline join-ids]])]
+     :join     [:collections [:= :issues.id :collections.item_id]]
+     :group-by :issues.id
+     :having   [:raw (str "COUNT(issues.id) = " (count join-ids))]})])
+
+(defn- order-by [events-view search-mode]
+  [(if (= 0 events-view)
+                 (if (or (= 2 search-mode) (= 3 search-mode ))
+                   [:issues.short_title_ints (if (= 2 search-mode)
+                                               :asc
+                                               :desc)]
+                   [:issues.updated_at (if (= 1 search-mode)  
+                                         :asc
+                                         :desc)])
+                 [:issues.date (if (= 1 events-view)
+                                 :asc
+                                 :desc)])])
+
+(defn- limit [{:keys [selected-context
+                      link-issue
+                      events-view
+                      q]}]
+  (when (or (and (= "" q)
+                  (not selected-context)
+                  (= 0 events-view))
+             link-issue)
+     {:limit 500}))
 
 (defn- wrap-given-issues-query-with-limit
   [issue-ids-to-remove
    {:keys [selected-context
            join-ids
-           link-issue
            search-mode
            events-view
            and-query?
@@ -42,8 +65,8 @@
               (vec (concat search.core/select [:collections.annotation]))
               search.core/select)
     :from   :issues
-    :where  [:and 
-             (when and-query? [:in :issues.id (and-query opts)])
+    :where  [:and
+             (when and-query? (and-query opts))
              (when issue-ids-to-remove
                 [:not [:in :issues.id [:inline issue-ids-to-remove]]])
              (get-search-clause q)
@@ -54,22 +77,8 @@
                true)
              (when (or (= 2 search-mode) (= 3 search-mode))
                [:> :short_title_ints 0])]}
-   {:order-by [(if (= 0 events-view)
-                 (if (or (= 2 search-mode) (= 3 search-mode ))
-                   [:issues.short_title_ints (if (= 2 search-mode)
-                                               :asc
-                                               :desc)]
-                   [:issues.updated_at (if (= 1 search-mode)  
-                                         :asc
-                                         :desc)])
-                 [:issues.date (if (= 1 events-view)
-                                 :asc
-                                 :desc)])]}
-   (when (or (and (= "" q)
-                  (not selected-context)
-                  (= 0 events-view))
-             link-issue)
-     {:limit 500})
+   {:order-by (order-by events-view search-mode)}
+   (limit opts)
    (when join-ids
      {:join [:collections [:= :issues.id :collections.item_id]]})))
 
