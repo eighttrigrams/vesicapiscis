059a3c456b3281b924cecb1ab1e02e85bbfc4796
Dan <dan@eighttrigrams.net>
2025-07-23 02:18:04 +0100

    Limit

--- src/et/vp/ds/search/items.clj
+++ src/et/vp/ds/search/items.clj
@@ -1,7 +1,8 @@
 (ns et.vp.ds.search.items
   (:require [et.vp.ds.search.core :as core]
             [et.vp.ds.search.helpers :as search.helpers]
-            [honey.sql :as sql]))
+            [honey.sql :as sql]
+            [cambium.core :as log]))
 
 (defn- get-exclusion-clause [{:keys [link-context selected-context]}]
   (when 
@@ -15,15 +16,16 @@
   [q {:keys [selected-context all-items?] :as opts}]
   (let [{:keys [limit]} opts
         exclusion-clause (get-exclusion-clause opts)]
-    (sql/format 
-     (merge {:select   core/select
-             :from     [:issues]
-             :where    [:and
-                        (search.helpers/get-search-clause q)
-                        (when-not all-items? [:= :issues.is_context true])
-                        (when selected-context [:not [:= :issues.id (:id selected-context)]])
-                        (when selected-context exclusion-clause)]
-             :order-by [[(if all-items? 
-                           :updated_at
-                           :updated_at_ctx) :desc]]
-             :limit (or limit 100)}))))
+    (log/info (str "fetch-items limit: " limit ". -> " (or limit 100)))
+    (sql/format  
+     {:select   core/select
+      :from     [:issues]
+      :where    [:and
+                 (search.helpers/get-search-clause q)
+                 (when-not all-items? [:= :issues.is_context true])
+                 (when selected-context [:not [:= :issues.id (:id selected-context)]])
+                 (when selected-context exclusion-clause)]
+      :order-by [[(if all-items? 
+                    :updated_at
+                    :updated_at_ctx) :desc]]
+      :limit (or limit 100)})))
