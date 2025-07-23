2778eca8c55324a094c7c92281692775bf459468
Dan <dan@eighttrigrams.net>
2025-07-23 12:46:56 +0100

    Move function

--- src/et/vp/ds/search/core.clj
+++ src/et/vp/ds/search/core.clj
@@ -10,13 +10,3 @@
              :issues.inserted_at
              :issues.updated_at
              :issues.date])
-
-(defn exclusion-clause [selected-context-id mode]
-  [:not [:in :issues.id 
-         (if (= :issues mode)
-           {:select :collections.item_id
-            :from   :collections
-            :where  [:= :collections.container_id [:inline selected-context-id]]}
-           {:select :collections.container_id
-            :from   :collections
-            :where  [:= :collections.item_id selected-context-id]})]])
--- src/et/vp/ds/search/items.clj
+++ src/et/vp/ds/search/items.clj
@@ -3,14 +3,24 @@
             [et.vp.ds.search.helpers :as search.helpers]
             [honey.sql :as sql]))
 
+(defn exclusion-clause [selected-context-id mode]
+  [:not [:in :issues.id 
+         (if (= :issues mode)
+           {:select :collections.item_id
+            :from   :collections
+            :where  [:= :collections.container_id [:inline selected-context-id]]}
+           {:select :collections.container_id
+            :from   :collections
+            :where  [:= :collections.item_id selected-context-id]})]])
+
 (defn fetch-items
   [q {:keys [selected-context all-items? link-context link-issue] :as opts}]
   (let [{:keys [limit]} opts
         exclusion-clause (when (or link-context link-issue)
-                           (core/exclusion-clause (:id (:selected-context opts))
-                                                  (if link-issue
-                                                    :issues
-                                                    :contexts)))]
+                           (exclusion-clause (:id (:selected-context opts))
+                                             (if link-issue
+                                               :issues
+                                               :contexts)))]
     (sql/format  
      {:select   core/select
       :from     [:issues]
